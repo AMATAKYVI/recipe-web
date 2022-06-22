59f6e24ab8b6a46f0f75ba06ace931cb706e750b
AMATAKYVI <yviamatak123@gmail.com>
2022-06-22 18:03:40 -0400

    mobile supported

--- components/BodyPart.js
+++ components/BodyPart.js
@@ -53,9 +53,7 @@ function BodyPart({ data }) {
               className="w-[50%] py-2 px-2 text-lg outline-none border rounded-t "
             />
           </form>
-          {newArraySearch[0]?.data?.length > 0 &&
-          inputValue != '' &&
-          inputValueMobile != '' ? (
+          {newArraySearch[0]?.data?.length > 0 && inputValue != '' ? (
             <div className="absolute top-15 border sm:left-[20.9%] bg-white w-[50%] transition-all duration-200">
               {newArraySearch[0]?.data?.map((item) => {
                 return (
@@ -69,9 +67,7 @@ function BodyPart({ data }) {
                 );
               })}
             </div>
-          ) : newArraySearch[0]?.data?.length == 0 &&
-            inputValue != '' &&
-            inputValueMobile != '' ? (
+          ) : newArraySearch[0]?.data?.length == 0 && inputValue != '' ? (
             <div className="absolute top-15 border  left-[20.9%] bg-white w-[50%] transition-all duration-200">
               No match
             </div>
