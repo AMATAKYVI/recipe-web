666f498837a5309a0fa31f79a7173bf09886cd67
AMATAKYVI <yviamatak123@gmail.com>
2024-08-06 23:41:51 -0400

    chore: Update npm dependencies and add @vercel/analytics package

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import Header from '../components/header/Header';
-
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className="max-w-screen-xl mx-auto">
         <Header />
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </>
   );
